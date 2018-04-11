4e23375b10e6c2680676e2e6ccad798d88bdda19
Michael Rutherford <michael.rutherford@eng.ox.ac.uk>
2018-04-11 17:07:57 +0100

    Added comments
    Added comments to CallPhaseRetrieval.m  to describe what each section of the code is doing

--- MonoFunction/CallPhaseRetrieval.m
+++ MonoFunction/CallPhaseRetrieval.m
@@ -2,9 +2,13 @@ close all
 clear all
 clc
 
+%% Load simulated phase contrast image and X-ray data
+
 data   = load('ID30keV_6mm_Al_cylinder_400px_M-1.mat');
 source = load('DataSource.mat');
 
-[thickness, phase, density] = function_PhaseRetrievalMono(data, source);
+%% Run phase retrieval
+
+[thickness, phase, density]                         = function_PhaseRetrievalMono(data, source);
       
-[thicknessTruePlot, phaseTruePlot, densityTruePlot]       = functionTrueValues(source);
+[thicknessTruePlot, phaseTruePlot, densityTruePlot] = functionTrueValues(source);
